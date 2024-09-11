81ee8b75a6351e5235dccb4d07aec3e5938c4a60
Haithem Amir <haithemamir@Haithems-MacBook-Pro.local>
2024-09-11 16:34:58 -0400

    Updated WordCounter.java with latest changes

--- LSP_Fall_2024/src/org/howard/edu/lsp/assignment2/WordCounter.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/assignment2/WordCounter.java
@@ -1,21 +1,44 @@
 package org.howard.edu.lsp.assignment2;
 
-import java.io.File;
+import java.io.InputStream;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WordCounter {
-
-	public static void main(String[] args) {
-        //Read Input File Fucntion
-        InputStream text = getClass().getResourceAsStream("words.txt");
+    public static void main(String[] args) {
+        // Read Input File Function using WordCounter class reference
+        InputStream text = WordCounter.class.getResourceAsStream("words.txt");
         Scanner sc = new Scanner(text);
- 
-        while (sc.hasNextLine())
-            System.out.println(sc.nextLine());
 
+        // HashMap to store word counts
+        Map<String, Integer> wordCount = new HashMap<>();
 
+        // Read and process each line of the file
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            
+            // Tokenize the line into words (not counting integers)
+            String[] words = line.split("[^a-zA-Z]+");
 
-        //Create Hash Map
-        //Parse through the file, Ignore words less than 3 letters and integers
+            for (String word : words) {
+                word = word.toLowerCase();  // Convert to lowercase 
+                
+                // Ignore trivial words (words with 3 or fewer characters)
+                if (word.length() > 3) {
+                    // Count the word occurrences in the map
+                    wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+                }
+            }
+        }
+
+        // Close the scanner
+        sc.close();
+
+        // Print the word counts
+        System.out.printf("%-15s %s%n", "Word", "Count");
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            System.out.printf("%-15s %d%n", entry.getKey(), entry.getValue());
+        }
     }
 }
