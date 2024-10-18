6cfe325482d6fb271e5050912411ff471ed58b51
haithemamir <122388788+haithemamir@users.noreply.github.com>
2024-10-17 20:34:23 -0400

    Update Book.java

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question1/Book.java
@@ -0,0 +1,69 @@
+package org.howard.edu.lsp.midterm.question1;
+
+public class Book {
+    // Attributes representing book details
+    private String title;
+    private String author;
+    private String ISBN;
+    private int yearPublished;
+
+    // Constructor to initialize Book object
+    public Book(String title, String author, String ISBN, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.yearPublished = yearPublished;
+    }
+
+    // Getters and setters for each attribute
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    // Compares books based on ISBN and author
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Book otherBook = (Book) obj;
+        return ISBN.equals(otherBook.ISBN) && author.equals(otherBook.author);
+    }
+
+    // Returns a string representation of the book
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", ISBN: " + ISBN + ", Year Published: " + yearPublished;
+    }
+}
