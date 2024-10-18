03560e6f31c725d0bec662373acf789b74422087
haithemamir <122388788+haithemamir@users.noreply.github.com>
2024-10-17 20:32:59 -0400

    Create Calculator.java

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question2/Calculator.java
@@ -0,0 +1,23 @@
+package org.howard.edu.lsp.midterm.question2;
+
+public class Calculator {
+
+    // Sum two integers
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    // Sum two double values
+    public static double sum(double a, double b) {
+        return a + b;
+    }
+
+    // Sum all elements in an array of integers
+    public static int sum(int[] numbers) {
+        int total = 0;
+        for (int num : numbers) {
+            total += num;
+        }
+        return total;
+    }
+}
